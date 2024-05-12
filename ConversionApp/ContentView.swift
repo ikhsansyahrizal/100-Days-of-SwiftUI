01bb27301b3d4680b8289c528185257c4b1c047b
Ikhsan Syahrizal <ikhsan.syahrizal@spesolution.com>
2024-05-12 16:03:37 +0700

    finish learning day 19

--- ConversionApp/ContentView.swift
+++ ConversionApp/ContentView.swift
@@ -8,14 +8,103 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var value1: Double = 0
+    @State private var type1: String = "mm"
+    @State private var type2: String = "mm"
+    let longFormat = ["mm", "cm", "dm", "m", "dk", "hm", "km"]
+    
+    private var convertToMm: Double {
+        let valueToConvert = value1
+        
+        switch type1 {
+        case "mm":
+            return valueToConvert
+        case "cm" :
+            return valueToConvert * 10
+        case "dm" :
+            return valueToConvert * 100
+        case "m" :
+            return valueToConvert * 1000
+        case "dk" :
+            return valueToConvert * 10000
+        case "hm" :
+            return valueToConvert * 100000
+        case "km" :
+            return valueToConvert * 1000000
+        default:
+            return 0
         }
-        .padding()
+    }
+    
+    var result: Double {
+        
+        switch type2 {
+        case "mm":
+            return convertToMm
+        case "cm" :
+            return convertToMm / 10
+        case "dm" :
+            return convertToMm / 100
+        case "m" :
+            return convertToMm / 1000
+        case "dk" :
+            return convertToMm / 10000
+        case "hm" :
+            return convertToMm / 100000
+        case "km" :
+            return convertToMm / 1000000
+        default:
+            return 0
+        }
+    }
+    
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Section() {
+                    HStack() {
+                        TextField("number to convert", value: $value1, format: .number)
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                            .keyboardType(.numberPad)
+                        Picker("Format type", selection: $type1) {
+                            ForEach(longFormat, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .padding()
+                    }
+                    
+                }
+                
+                Section("Convert to ▼") {
+                    HStack() {
+                        Text(result, format: .number)
+                            .frame(maxWidth: .infinity,alignment: .leading)
+                            .padding()
+                        Picker("Format type", selection: $type2) {
+                            ForEach(longFormat, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .padding()
+                    }
+                }
+                
+                
+            }
+            .padding()
+            .navigationTitle("Conversion App")
+            
+            Image("fallout")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+        }
+        
     }
 }
 
