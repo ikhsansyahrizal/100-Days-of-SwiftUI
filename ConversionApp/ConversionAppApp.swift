41db06e1304c2c59b074b3c0afb812fd9073dde8
Ikhsan Syahrizal <ikhsan.syahrizal@spesolution.com>
2024-05-12 09:54:29 +0700

    setup new project for day-19

---
+++ ConversionApp/ConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionAppApp.swift
+//  ConversionApp
+//
+//  Created by Ikhsan on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
